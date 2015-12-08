754b34da848b119a6a6aa5ef94a51fd655685c7d
Pierre Arnaud <arnaud@epsitec.ch>
2015-12-08 15:17:08 +0100

    Hot reload with devserver.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 'use strict';
 
 import React from 'react';
+import ReactDOM from 'react-dom';
 
-React.render (
+ReactDOM.render (
   <div>
     <h1>Electrum</h1>
-    <p>That's it...</p>
+    <p>That's it, this is the <b>root</b> component.</p>
   </div>,
   document.getElementById ('root')
 );
--- webpack.config.js
+++ webpack.config.js
@@ -2,41 +2,48 @@
 
 var webpack = require ('webpack');
 var path = require ('path');
-var root = './src';
+var output = './webpack';
 
 module.exports = {
   target: 'web',
   debug: true,
   devtool: 'eval',
+
   entry: [
     'webpack-dev-server/client?http://localhost:3000',
     'webpack/hot/only-dev-server',
     './src/index.js'
   ],
+
   output: {
-    publicPath: 'http://localhost:3000/',
-    path: path.resolve (root),
     filename: 'bundle.js',
-    hotUpdateChunkFilename: 'hot/[id].[hash].hot-update.js',
-    hotUpdateMainFilename: 'hot/[hash].hot-update.json'
+    path: path.resolve (output),
+    publicPath: 'http://localhost:3000/',
+    // hotUpdateChunkFilename: 'hot/[id].[hash].hot-update.js',
+    // hotUpdateMainFilename: 'hot/[hash].hot-update.json'
   },
-  recordsPath: path.resolve (path.join (root, 'webpack/webpack.records.json')),
+
+  // recordsPath: path.resolve (path.join (output, 'webpack/webpack.records.json')),
+
   resolve: {
-    modulesDirectories: ['node_modules']
+    modulesDirectories: ['node_modules'],
+    extensions: ['', '.js']
   },
-  module: {
-    loaders: [
-      {test: /\.css$/, loader: 'style-loader!css-loader'},
-      {test: /\.less$/, loader: 'style-loader!css-loader!less-loader'},
-      {test: /\.js$/,  exclude: /node_modules|jquery/, loaders: ['react-hot', 'babel-loader']},
-      {test: /\.png$/, loader: 'url-loader?limit=100000&mimetype=image/png'},
-      {test: /\.gif$/, loader: 'url-loader?limit=100000&mimetype=image/gif'},
-      {test: /\.json$/, loader: 'json-loader'},
-      {test: /\.(jpg|ttf|eot|svg|woff)/, loader: 'file-loader'}
-    ]
-  },
+
   plugins: [
     new webpack.HotModuleReplacementPlugin (),
     new webpack.NoErrorsPlugin ()
-  ]
+  ],
+
+  module: {
+    loaders: [
+      {test: /\.js$/,  exclude: /node_modules|jquery/, loaders: ['react-hot', 'babel-loader']},
+/*    {test: /\.css$/, loader: 'style-loader!css-loader'},
+      {test: /\.less$/, loader: 'style-loader!css-loader!less-loader'},
+      {test: /\.png$/, loader: 'url-loader?limit=100000&mimetype=image/png'},
+      {test: /\.gif$/, loader: 'url-loader?limit=100000&mimetype=image/gif'},
+      {test: /\.json$/, loader: 'json-loader'},
+      {test: /\.(jpg|ttf|eot|svg|woff)/, loader: 'file-loader'} */
+    ]
+  },
 };
